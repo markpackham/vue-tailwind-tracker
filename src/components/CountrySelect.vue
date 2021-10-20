0804a298faf988fc4e38b17034a4fe4ddfb973f8
mark <markpackham1@gmail.com>
2021-10-20 12:03:11 +0100

    apply clear option

--- src/components/CountrySelect.vue
+++ src/components/CountrySelect.vue
@@ -1,11 +1,11 @@
 <template>
   <select
-    @changge="onChange"
+    @change="onChange"
     v-model="selected"
     class="form-select mt-10 block w-full border p-3 rounded"
   >
     <option value="0">Select Country</option>
-    <option v-for="country in countries" :key="country.ID" :value="country.ID">
+    <option v-for="country in countries" :value="country.ID">
       {{ country.Country }}
     </option>
   </select>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,6 +4,7 @@
     <DataBoxes :stats="stats" />
     <CountrySelect @get-country="getCountryData" :countries="countries" />
     <button
+      @click="clearCountryData"
       v-if="stats.Country"
       class="
         bg-green-700
@@ -55,6 +56,13 @@ export default {
     getCountryData(country) {
       this.stats = country;
       this.title = country.Country;
+    },
+    async clearCountryData() {
+      this.loading = true;
+      const data = await this.fetchCovidData();
+      this.title = "Global";
+      this.stats = data.Global;
+      this.loading = false;
     },
   },
   async created() {
