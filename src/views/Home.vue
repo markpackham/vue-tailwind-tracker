28eaab940e26fded12d5c5918a5e1a202e9e7cf9
mark <markpackham1@gmail.com>
2021-10-20 11:53:56 +0100

    adding clear button
    https://youtu.be/m-MAIpnH9ag?t=2751

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,21 @@
   <main v-if="!loading">
     <DataTitle :dataDate="dataDate" :text="title" />
     <DataBoxes :stats="stats" />
+    <CountrySelect @get-country="getCountryData" :countries="countries" />
+    <button
+      v-if="stats.Country"
+      class="
+        bg-green-700
+        text-white
+        rounded
+        p-3
+        mt-10
+        focus:outline-none
+        hover:bg-green-600
+      "
+    >
+      Clear Country
+    </button>
   </main>
   <main v-else class="flex flex-col align-center justify-center text-center">
     <div class="text-gray-500 text-3xl mt-10 mb-6">Fetching Data</div>
@@ -12,12 +27,14 @@
 <script>
 import DataTitle from "@/components/DataTitle";
 import DataBoxes from "@/components/DataBoxes";
+import CountrySelect from "@/components/CountrySelect";
 
 export default {
   name: "Home",
   components: {
     DataTitle,
     DataBoxes,
+    CountrySelect,
   },
   data() {
     return {
@@ -34,6 +51,10 @@ export default {
       const res = await fetch(`https://api.covid19api.com/summary`);
       const data = await res.json();
       return data;
+    },
+    getCountryData(country) {
+      this.stats = country;
+      this.title = country.Country;
     },
   },
   async created() {
