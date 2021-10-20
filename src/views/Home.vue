46e7359976e90a0d9d239dd7310765cd51c44d05
mark <markpackham1@gmail.com>
2021-10-20 10:41:19 +0100

    output data date
    https://youtu.be/m-MAIpnH9ag?t=1531

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,44 @@
 <template>
-  <main>
+  <main v-if="!loading">
+    <DataTitle :dataDate="dataDate" :text="title" />
+  </main>
+  <main v-else class="flex flex-col align-center justify-center text-center">
     <div class="text-gray-500 text-3xl mt-10 mb-6">Fetching Data</div>
+    <img :src="require('../assets/hourglass.gif')" alt="" class="w-24 m-auto" />
   </main>
 </template>
 
 <script>
+import DataTitle from "@/components/DataTitle";
+
 export default {
   name: "Home",
-  components: {},
+  components: {
+    DataTitle,
+  },
+  data() {
+    return {
+      loading: true,
+      title: "Global",
+      dataDate: "",
+      stats: {},
+      countries: [],
+      loadingImage: require("../assets/hourglass.gif"),
+    };
+  },
+  methods: {
+    async fetchCovidData() {
+      const res = await fetch(`https://api.covid19api.com/summary`);
+      const data = await res.json();
+      return data;
+    },
+  },
+  async created() {
+    const data = await this.fetchCovidData();
+    this.dataDate = data.Date;
+    this.stats = data.Global;
+    this.countries = data.Countries;
+    this.loading = false;
+  },
 };
 </script>
